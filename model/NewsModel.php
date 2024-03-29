701b69357964e4cb9132b383b9f82dc8489f102c
Tevin01 <tevin.eliya.bofona@cf2000.onmicrosoft.com>
2024-03-29 10:00:25 +0100

    Yes

--- model/NewsModel.php
+++ model/NewsModel.php
@@ -1,8 +1,4 @@
 <?php
 
-function UnhappyAt4PM() {
-    $sql = "SELECT * `news` ON LEFT JOIN FROM `thename` ORDER BY ASC";
-    $query-> 
-    // Since all of this is probably wrong, I'll see it done tomorrow).
 
-}
+?>
