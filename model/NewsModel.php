5a6e1ecd2e1449abe3926d824c6adc72e29471b8
Tevin01 <tevin.eliya.bofona@cf2000.onmicrosoft.com>
2024-04-08 13:26:03 +0200

    exe4

--- model/NewsModel.php
+++ model/NewsModel.php
@@ -1,4 +1,39 @@
 <?php
 
+function getAllNewsHomePage(PDO $connect): array|string
+{
+    $sql = "SELECT n.`title`, n.`slug`, SUBSTRING(n.`content`, 1, 280) AS content, n.`date_published`, 
+                   u.`login`, u.`thename`
+	FROM `news` n
+	LEFT JOIN `user` u
+		ON n.`user_iduser` = u.`iduser`
+-- on va sélectionner les champs title (as categ_title) et slug (as categ_slug) de la table category qu'il y ait des catégories ou pas
 
-?>
+    LEFT JOIN `title` AS `categ_title` AND `slug`, AS `categ_slug` FROM new_has_category ON category
+
+    
+
+-- Condition de récupération
+    WHERE n.`is_published` = 1
+    ORDER BY n.`date_published` DESC
+
+        ;";
+
+    try{
+    
+    $query = $connect->query($sql);
+
+    // si pas de résultats () : string
+    if(!$query->rowCount()) return "Pas encore de message";
+    
+    $result = $query->fetchAll();
+
+    $query->closeCursor();
+
+    return $result; // : array
+
+    }catch(Exception $e){
+        return $e->getMessage(); // : string
+    }
+
+}
